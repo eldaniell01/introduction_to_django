7a15d885d3b3214ec66320e02c055c16ebd185e5
eldaniell01 <amontepequem@miumg.edu.gt>
2025-09-07 17:46:48 -0600

    registro de usuario

--- hello/bookstore_project/bookstore_project/urls.py
+++ hello/bookstore_project/bookstore_project/urls.py
@@ -10,7 +10,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLcon,
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('users.urls')),
+    path('', include('pages.urls')),
 ]
