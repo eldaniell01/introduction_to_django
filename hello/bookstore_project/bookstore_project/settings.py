1d4db0c805a13915340fba414d93aec95d8fcf19
eldaniell01 <amontepequem@miumg.edu.gt>
2025-09-16 22:38:50 -0600

    debug y secret_key

--- hello/bookstore_project/bookstore_project/settings.py
+++ hello/bookstore_project/bookstore_project/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ib$dj5j#la7671+x5cz)+d@k8xqb346j3giok3)^z=!#t&8=%7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = int(os.environ.get('DEBUG', default=0))
 ALLOWED_HOSTS = []
 
 
