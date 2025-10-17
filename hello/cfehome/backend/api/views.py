c4b24e0778d7db41329579e633f2edc69fd65fcc
eldaniell01 <amontepequem@miumg.edu.gt>
2025-10-17 02:01:52 -0600

    serializacion

--- hello/cfehome/backend/api/views.py
+++ hello/cfehome/backend/api/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 # Create your views here.
 
-def api_home(reques, *args, **kwargs):
-    return JsonResponse({"message": "hola, esta es la api"})
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by('?').first()
+    data = {}
+    if instance:
+        #data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
